3794d671fdb00ce57a460fe54d62e989aacddd9f
juanitooxx2 <juanteruel11101@gmail.com>
2025-01-13 10:52:42 +0100

    ejercicio de pepe

---
+++ alo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -e "Hola Mundo"
+echo "Dime tu usuario"
+read "Usuario"
+echo "Bienvenido al curso de bash"
---
+++ juan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#alo 
+echo -e "Hola/n Mundo"
+printf "%-10s | %-5s\n" "Jhon" "Edad"
+printf "%-10s | %-5d\n" "Juan" 12
+printf "%-10s | %-5d\n" "Paco" 13
+
+
+echo $num1
+echo $num
+num1=2
+echo $num1
+num=1
+echo $num
+echo "Pon un numero:"
+
+read num
+echo "El num que has puesto es: $num"
